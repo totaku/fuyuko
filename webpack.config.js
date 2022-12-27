a52a8bbd581a5bb1ecb85c01af2e1e90284604bc
Totaku <banana@3lf.ru>
2022-12-28 00:40:38 +0300

    add style

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
@@ -19,6 +20,15 @@ module.exports = {
                 test: /\.pug$/,
                 loader: 'pug-loader',
             },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
         ],
     },
     plugins: [
@@ -32,6 +42,9 @@ module.exports = {
                     delete: ['dist'],
                 },
             },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
         }),
     ],
     devServer: {
